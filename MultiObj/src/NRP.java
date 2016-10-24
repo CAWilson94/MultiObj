dbfb6c7654c998db8b6714794cdb35f159c9586f
Charlotte Wilson <charlotteawilson.94@gmail.com>
2016-10-24 07:46:55 +0100

    nrp cost, levels and num requirment attributes added

--- MultiObj/src/NRP.java
+++ MultiObj/src/NRP.java
@@ -1,8 +1,41 @@
 /**
  * NRP instance to be generated
+ * 
  * @author Charlotte Wilson
  *
  */
 public class NRP {
+	
+	private int reqLevel = 0;
+	private int numReq = 0;
+	private int reqCost = 0;
+	
+	public NRP(int reqLevel, int numReq, int reqCost){
+		
+	}
+
+	public int getReqLevel() {
+		return reqLevel;
+	}
+
+	public void setReqLevel(int reqLevel) {
+		this.reqLevel = reqLevel;
+	}
+
+	public int getNumReq() {
+		return numReq;
+	}
+
+	public void setNumReq(int numReq) {
+		this.numReq = numReq;
+	}
+
+	public int getReqCost() {
+		return reqCost;
+	}
+
+	public void setReqCost(int reqCost) {
+		this.reqCost = reqCost;
+	}
 
 }
