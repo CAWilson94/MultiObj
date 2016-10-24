8dd6a0f7899d5ef474d42eb0dd3cf58432cd64dd
Charlotte Wilson <charlotteawilson.94@gmail.com>
2016-10-24 21:32:31 +0100

    renamed

---
+++ MultiObj/src/Customer.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer {
+
+	int profit = 0;
+	int requests = 0;
+	List<Integer> reqList = new ArrayList<Integer>();
+
+	/**
+	 * Individual Customers
+	 * @param profit
+	 * @param requests
+	 * @param reqList
+	 */
+	public Customer(int profit, int requests, List<Integer> reqList) {
+		this.profit = profit;
+		this.requests = requests;
+		this.reqList = reqList;
+	}
+}
--- MultiObj/src/NRP.java
+++ MultiObj/src/NRP.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * NRP instance to be generated
  * 
@@ -16,49 +19,157 @@ public class NRP {
 	private int reqCost = 0;
 	private int dependancies = 0;
 	private int numCustomers = 0;
+	// Requirements list for A column
+	private List<Integer> IDReqA = new ArrayList<Integer>();
+	// Requirements list for B column
+	private List<Integer> IDReqB = new ArrayList<Integer>();
 
+	/**
+	 * Basically I am lazy and would like the option to make this in any
+	 * destructive manner I wish
+	 */
+	public NRP() {
+
+	}
+
+	/**
+	 * NRP with all attributes specified
+	 * 
+	 * @param reqLevel
+	 * @param numReq
+	 * @param reqCost
+	 * @param dependancies
+	 * @param numCustomers
+	 */
 	public NRP(int reqLevel, int numReq, int reqCost, int dependancies, int numCustomers) {
 
 	}
 
+	/**
+	 * Get requirement level max i.e. total
+	 * 
+	 * @return int reqLevel
+	 */
 	public int getReqLevel() {
 		return reqLevel;
 	}
 
+	/**
+	 * Set requirement level: should never need this...
+	 * 
+	 * @param reqLevel
+	 */
 	public void setReqLevel(int reqLevel) {
 		this.reqLevel = reqLevel;
 	}
 
+	/**
+	 * Get number of requirements
+	 * 
+	 * @return int numReq
+	 */
 	public int getNumReq() {
 		return numReq;
 	}
 
+	/**
+	 * Set number of requirements: should never need this..
+	 * 
+	 * @param numReq
+	 */
 	public void setNumReq(int numReq) {
 		this.numReq = numReq;
 	}
 
+	/**
+	 * Get cost for a given requirement
+	 * 
+	 * @return int reqCost
+	 */
 	public int getReqCost() {
 		return reqCost;
 	}
 
+	/**
+	 * Set cost for a given requirement: should never need this..
+	 * 
+	 * @param reqCost
+	 */
 	public void setReqCost(int reqCost) {
 		this.reqCost = reqCost;
 	}
 
+	/**
+	 * Get number of dependencies for overall NRP file
+	 * 
+	 * @return int dependencies
+	 */
 	public int getDependancies() {
 		return dependancies;
 	}
 
+	/**
+	 * Set the number of dependencies: shouldn't need this unless testing .. or
+	 * you are just wild
+	 * 
+	 * @param dependancies
+	 */
 	public void setDependancies(int dependancies) {
 		this.dependancies = dependancies;
 	}
 
+	/**
+	 * Get number of customers
+	 * 
+	 * @return int numCustomers
+	 */
 	public int getNumCustomers() {
 		return numCustomers;
 	}
 
+	/**
+	 * Set number customers: usual warning..
+	 * 
+	 * @param numCustomers
+	 */
 	public void setNumCustomers(int numCustomers) {
 		this.numCustomers = numCustomers;
+	}
+
+	/**
+	 * Get the ID's for column A of file
+	 * 
+	 * @return List<Integer> IDReqA
+	 */
+	public List<Integer> getIDReqA() {
+		return IDReqA;
+	}
+
+	/**
+	 * Set ID's for column A of file: usual warning..
+	 * 
+	 * @param iDReqA
+	 */
+	public void setIDReqA(List<Integer> iDReqA) {
+		IDReqA = iDReqA;
+	}
+
+	/**
+	 * Get the ID's for column B of file
+	 * 
+	 * @return List<Integer> IDReqB
+	 */
+	public List<Integer> getIDReqB() {
+		return IDReqB;
+	}
+
+	/**
+	 * Set ID's for column A of file: usual warning..
+	 *  
+	 * @param iDReqB
+	 */
+	public void setIDReqB(List<Integer> iDReqB) {
+		IDReqB = iDReqB;
 	}
 
 }
