387334ebc4b74fe17cb6089c9685f101f04b9440
Charlotte Wilson <charlotteawilson.94@gmail.com>
2016-10-31 07:28:45 +0000

    Stumbling my way through this..

--- MultiObj/src/Creator.java
+++ MultiObj/src/Creator.java
@@ -2,9 +2,30 @@
  * Class for creating our population. The only thing here is.. wrtf do I want a
  * population of?
  * 
+ * Need chromosomes of individuals remember
+ * 
  * @author Charlotte Wilson
  *
  */
 public class Creator {
+
+	NRP nrp = new NRP();
+
+	/**
+	 * Will need to call the parse file before making an nrp object?
+	 * 
+	 * @param nrp
+	 */
+	public Creator(NRP nrp) {
+		this.nrp = nrp;
+	}
+
+	public void createIndi() {
+
+	}
+	
+	public void createChromo(){
+		
+	}
 
 }
