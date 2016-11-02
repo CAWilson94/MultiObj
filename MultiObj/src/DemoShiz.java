27bc5722fd97bc75e121cc3163e5ada30e81e65e
Charlotte Wilson <charlotteawilson.94@gmail.com>
2016-11-02 10:03:30 +0000

    seeing if requirement has been picked or not

--- MultiObj/src/DemoShiz.java
+++ MultiObj/src/DemoShiz.java
@@ -1,4 +1,29 @@
+import org.opt4j.core.genotype.BooleanGenotype;
 
 public class DemoShiz {
 
+	public static void main(String[] args) {
+		FileParser fp = new FileParser();
+		NRP nrp = fp.parseFile();
+
+		NRPCreator c = new NRPCreator();
+		NRPDecoder d = new NRPDecoder();
+		BooleanGenotype boop = c.create();
+		System.out.println(boop);
+		String booping = d.decode(boop);
+		System.out.println(booping);
+		char[] yermaw = booping.toCharArray();
+
+		for (Customer n : nrp.getCustomers()) {
+			int j = 0;
+			for (int i = 0; i < n.getReqList().size(); i++) {
+				
+				if (yermaw[j] == '0') {
+					System.out.println("boop");
+				}
+				
+				j++;
+			}
+		}
+	}
 }
