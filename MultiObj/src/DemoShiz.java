8e5dced5c7351184f5a16a01cd3516b3daf733d4
Charlotte Wilson <charlotteawilson.94@gmail.com>
2016-11-03 17:47:51 +0000

    got requirments from customers and customer weight

--- MultiObj/src/DemoShiz.java
+++ MultiObj/src/DemoShiz.java
@@ -4,6 +4,18 @@ import java.util.List;
 
 import org.opt4j.core.genotype.BooleanGenotype;
 
+/**
+ * 
+ * @author Charlotte Wilson
+ * 
+ *         To evaluate the score: Weight = profit from customer Value customer
+ *         places on requirement = position of requirement in reqList
+ * 
+ *         Thus, you want to go through all customers and do : weight * value
+ *         Then sum dat shit together
+ *
+ */
+
 public class DemoShiz {
 
 	public List<Integer> ints = new ArrayList<Integer>();
@@ -28,21 +40,44 @@ public class DemoShiz {
 
 		int score = 0;
 		int weight = 0;
-		int profit = 0;
+		int value = 0;
 
 		for (int k = 0; k < nrp.getCustomers().size(); k++) {
 			int j = 0;
 			for (int i = 0; i < nrp.getCustomers().get(k).getReqList().size(); i++) {
 				if (phenotype.charAt(j) == '1') {
-					weight = j;
-					profit = nrp.getCustomers().get(k).getProfit();
-					score = weight * profit;
+					System.out.println("phenotype pos is: " + phenotype.charAt(j));
+					System.out.println("customer position is: " + i);
+					System.out.println("Need to search all customers for " + j);
+					weight = nrp.getCustomers().get(k).getProfit();
+					score = weight * value; // Assuming this is correct
 					System.out.println("score is: " + score);
-
 				}
 				j++;
 			}
 		}
+	}
+
+	public void halp(String phenotype, NRP nrp) {
+		List<Customer> dupReqs = new ArrayList<Customer>();
+		for (int i = 0; i < phenotype.length(); i++) {
+			if (phenotype.charAt(i) == '1') {
+				huntforReq(i, nrp);
+			}
+		}
+	}
+
+	private Customer huntforReq(int position, NRP nrp) {
+		List<Customer> customerList = nrp.getCustomers();
+		int i = 0;
+		for (Customer n : customerList) {
+			if (n.getReqList().contains(position)) {
+				System.out.println("found a requirement: " + i + " weight of customer is: " + n.getProfit());
+			}
+			// Store all into a temp array to work on after?
+			i++;
+		}
+		return null;
 	}
 
 	public static void main(String[] args) {
@@ -71,15 +106,8 @@ public class DemoShiz {
 
 		DemoShiz d1 = new DemoShiz();
 
-		d1.getWeightCustom(nrp.getCustomers());
-
-		System.out.println("score testing");
-
-		d1.score(booping);
-
-		System.out.println("score shit now");
-		d1.scoreShit(booping, nrp);
-
+		System.out.println("requirement hunting\n");
+		d1.huntforReq(104, nrp);
 	}
 
 }
