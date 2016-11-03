4625edacc1e6a759fe29fe592605ad72fffca2f9
Charlotte Wilson <charlotteawilson.94@gmail.com>
2016-11-03 21:54:17 +0000

    FOR THE ORDEER

--- MultiObj/src/DemoShiz.java
+++ MultiObj/src/DemoShiz.java
@@ -18,7 +18,7 @@ import org.opt4j.core.genotype.BooleanGenotype;
 
 public class DemoShiz {
 
-	public void halp(String phenotype, NRP nrp) {
+	public void finalScore(String phenotype, NRP nrp) {
 		List<Integer> finalScores = new ArrayList<Integer>();
 		for (int i = 0; i < phenotype.length(); i++) {
 			if (phenotype.charAt(i) == '1') {
@@ -26,6 +26,8 @@ public class DemoShiz {
 				finalScores.addAll(huntforReqScore(i, nrp));
 			}
 		}
+		System.out.println(finalScores.toString());
+		// TODO: some scores are 0 wtf??
 	}
 
 	/**
@@ -46,20 +48,17 @@ public class DemoShiz {
 
 				if (Constants.DEBUG_SCORES == true) {
 					System.out.println("r: " + position + " wc: " + n.getProfit() + " v: "
-							+ customerValueOnReq(position, n.getReqList()) + " :  " + n.getCustomerLabel());
+							+ customerValueOnReq(position, n.getReqList()));
 				}
 				// TODO: check this is right
 				score += n.getProfit() * customerValueOnReq(position, n.getReqList());
-				System.out.println(score + " is the score");
 				// TODO: Need to get the duplicates and sum the score of them
 				// all together!
 			}
-
 			i++;
 		}
 
 		scoresCurrent.add(score);
-
 		return scoresCurrent;
 	}
 
@@ -73,9 +72,9 @@ public class DemoShiz {
 	private int customerValueOnReq(int requirement, List<Integer> reqList) {
 		// TODO: write a version of this that works for the realistic data set
 		int weight = 0;
-		int fraction = reqList.size() + 1;
+		int fraction = 100 / reqList.size();
 		int reqPos = reqList.indexOf(requirement);
-		weight = 100 - reqPos * fraction;
+		weight = 100 - (reqPos * fraction);
 		return weight;
 	}
 
@@ -92,8 +91,7 @@ public class DemoShiz {
 
 		DemoShiz d1 = new DemoShiz();
 		System.out.println("now for scores: ");
-		// d1.halp(booping, nrp);
-		System.out.println(d1.huntforReqScore(128, nrp));
+		d1.finalScore(booping, nrp);
 	}
 
 }
